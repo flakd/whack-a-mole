d421254db4bc40bb78fa7742a6ff4d1bad7dbd39
Flak DiNenno <flak@dinenno.com>
2023-06-01 09:40:34 -0400

    got both PAUSE and GAME OVER working.  Separating styles for ios and web (can't see moles on web)

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -1,20 +1,37 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, ImageBackground} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ImageBackground,
+  TouchableOpacity,
+  Alert,
+  Modal,
+} from 'react-native';
 import Square from './Square';
 
 const GameBoard = (props) => {
-  const time = 60;
+  const time = 10;
   const [timeLeft, setTimeLeft] = useState(time);
   const [score, setScore] = useState(0);
+  const [isGameOver, setIsGameOver] = useState(false);
+  const [isGamePaused, setIsGamePaused] = useState(false);
+  const [justPaused, setJustPaused] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
 
   useEffect(() => {
-    if (!timeLeft) return;
-    const timerId = setInterval(() => {
-      //happens every 1 sec/1000ms
-      setTimeLeft(timeLeft - 1);
-    }, 1000);
-    return () => clearInterval(timerId);
-  }, [timeLeft]);
+    if (!isGamePaused) {
+      if (timeLeft > 0) {
+        const timerId = setInterval(() => {
+          //happens every 1 sec/1000ms
+          setTimeLeft(timeLeft - 1);
+        }, 1000);
+        return () => clearInterval(timerId);
+      } else if (timeLeft === 0) {
+        setIsGameOver(true);
+      }
+    }
+  }, [timeLeft, isGamePaused, isGameOver]);
 
   const squareArray = Array(12);
   for (let i = 0; i < squareArray.length; i++) {
@@ -28,30 +45,112 @@ const GameBoard = (props) => {
   };
 
   return (
-    <ImageBackground
-      style={styles.container}
-      source={require('../assets/background.png')}
-    >
-      <Text style={styles.header}>Whack-a-mole</Text>
-      <Text style={styles.score}>TIME: {timeLeft}</Text>
-      <Text style={styles.score}>SCORE: {score}</Text>
-      <View style={styles.game}>
-        {squareArray.map((square) => (
-          <Square
-            key={square}
-            id={square}
-            time={time}
-            addToScore={addToScore}
-          ></Square>
-        ))}
-      </View>
-    </ImageBackground>
+    <>
+      <Modal
+        id='gamePausedModal'
+        visible={isModalVisible}
+        transparent={true}
+        style={styles.modal}
+      >
+        <View style={styles.modal}>
+          <View style={styles.continueButtonBox}>
+            <Text style={styles.paused}>GAME</Text>
+            <Text style={styles.paused}>PAUSED</Text>
+            <TouchableOpacity
+              style={[styles.pauseButton, {backgroundColor: 'green'}]}
+              onPress={() => {
+                console.log('wtf');
+                setIsGamePaused(false);
+                setIsModalVisible(false);
+              }}
+            >
+              <Text style={styles.buttonText}>CONTINUE</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <Modal
+        id='gameOverModal'
+        visible={isGameOver}
+        transparent={true}
+        style={styles.modal}
+      >
+        <View style={styles.modal}>
+          <View style={styles.continueButtonBox}>
+            <Text style={styles.paused}>GAME</Text>
+            <Text style={styles.paused}>OVER</Text>
+            <TouchableOpacity
+              style={[styles.pauseButton, {backgroundColor: 'green'}]}
+              onPress={() => {
+                console.log('Game Over Pressed');
+                setIsGameOver(false);
+                setTimeLeft(10);
+                //setIsModalVisible(false);
+              }}
+            >
+              <Text style={styles.buttonText}>START</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+      <ImageBackground
+        style={styles.container}
+        source={require('../assets/background.png')}
+      >
+        <View style={styles.buttonBox}>
+          <TouchableOpacity
+            style={[
+              styles.pauseButton,
+              isGamePaused
+                ? {backgroundColor: '#5fb8e0'}
+                : {backgroundColor: 'blue'},
+            ]}
+            disabled={isGamePaused}
+            onPress={() => {
+              setIsGamePaused(true);
+              setIsModalVisible(true);
+            }}
+          >
+            <Text
+              style={[
+                styles.buttonText,
+                isGamePaused ? {color: '#92e8e0'} : {color: 'white'},
+              ]}
+            >
+              PAUSE
+            </Text>
+          </TouchableOpacity>
+        </View>
+        {/* //END ButtonBox  */}
+
+        <Text style={styles.header}>Whack-a-mole</Text>
+        <Text style={styles.score}>
+          TIME: {timeLeft} {'  '} SCORE: {score}
+        </Text>
+        <View style={[styles.game]}>
+          {squareArray.map((square) => (
+            <Square
+              key={square}
+              id={square}
+              time={time}
+              isGameOver={isGameOver}
+              setIsGameOver={setIsGameOver}
+              isGamePaused={isGamePaused}
+              addToScore={addToScore}
+            ></Square>
+          ))}
+        </View>
+      </ImageBackground>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    //flexDirection: 'column',
+    //flexWrap: 'wrap',
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     //justifyContent: 'center',
@@ -64,17 +163,67 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     width: '100%',
-    paddingTop: 50,
+    justifyContent: 'center',
+    alignSelf: 'center',
+
+    marginTop: 280,
+    position: 'absolute',
+  },
+  containerWeb: {
+    flex: 1,
+    //flexDirection: 'column',
+    //flexWrap: 'wrap',
+    //width: '100%',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignSelf: 'center',
+    //paddingTop: 70,
+    //marginLeft: 'auto',
+    //marginRight: 'auto',
+  },
+  gameWeb: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    width: 400,
+    justifyContent: 'center',
+    //alignItems: 'center',
+    alignSelf: 'center',
+
+    //marginTop: 280,
+    //position: 'absolute',
+  },
+  modal: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0 , 0.5)',
+    justifyContent: 'center',
+    width: '100%',
+    alignItems: 'center',
+    alignSelf: 'center',
+    margin: 'auto',
   },
   header: {
     fontWeight: 'bold',
     fontSize: 25,
     marginBottom: 10,
-    marginTop: 5,
-    textShadowColor: 'black',
-    textShadowOffset: {width: 1, height: 1},
-    shadowOpacity: 0.3,
-    shadowRadius: 1,
+    marginTop: 20,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 0.5,
+  },
+  paused: {
+    fontWeight: 'bold',
+    alignSelf: 'center',
+    fontSize: 25,
+    marginBottom: 5,
+    marginTop: 0,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.5,
+    shadowRadius: 0.5,
+    top: 0,
   },
   score: {
     fontWeight: 'bold',
@@ -83,9 +232,71 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     marginTop: 5,
     textShadowColor: 'black',
-    textShadowOffset: {width: 1, height: 1},
-    shadowOpacity: 1,
-    shadowRadius: -3,
+    textShadowOffset: {width: 0, height: 3},
+    textShadowRadius: 2,
+  },
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0 , 0.5)',
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+  },
+  continueButtonBox: {
+    width: 200,
+    height: 150,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    //alignItems: 'center',
+    //alignSelf: 'center',
+    margin: 'auto',
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 0},
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+    //position: 'absolute',
+    top: -170,
+  },
+  continueButton: {
+    //zIndex: 99,
+    //flexDirection: 'row',
+    //margin: 10,
+    //width: 120,
+    //alignSelf: 'center',
+    //position: 'absolute',
+    //top: 40,
+    //borderRadius: 10,
+    height: 35,
+    minWidth: 70,
+    borderRadius: 10,
+    margin: 5,
+    padding: 5,
+    top: 40,
+  },
+  buttonBox: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end',
+    //alignSelf: 'center',
+    position: 'absolute',
+    top: 30,
+    left: 220,
+  },
+  pauseButton: {
+    borderRadius: 10,
+    height: 35,
+    minWidth: 70,
+    margin: 5,
+    padding: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    textAlign: 'center',
+    //verticalAlign: 'center',
+    marginTop: 'auto',
+    marginBottom: 'auto',
   },
 });
 export default GameBoard;
--- components/Square.jsx
+++ components/Square.jsx
@@ -3,24 +3,40 @@ import {View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
 
 const Square = (props) => {
   const [moleActive, setMoleActive] = useState(false);
-  const [isGameOver, setIsGameOver] = useState(false);
+  //const [isGameOver, setIsGameOver] = useState(false);
 
   const randomTime = Math.random() * 20000;
+
+  const randomIntFromInterval = (min, max) => {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+  const rndInt = randomIntFromInterval(2000, 20000);
+  //console.log(rndInt);
+
   let timerId = null;
 
   useEffect(() => {
-    timerId = setInterval(() => {
-      setMoleActive(true);
-      setTimeout(() => {
-        setMoleActive(false);
-      }, 800);
-    }, randomTime);
-    setTimeout(endGame, props.time * 1000);
-  }, []);
+    if (!props.isGamePaused) {
+      timerId = setInterval(() => {
+        setMoleActive(true);
+        setTimeout(() => {
+          setMoleActive(false);
+        }, 800);
+      }, rndInt);
+      if (props.isGameOver) endGame();
+      //this should in 60 secs from GameBoard.jsx
+      //setTimeout(endGame, props.time * 1000);
+    } else {
+      return;
+    }
+  }, [props.isGamePaused, props.isGameOver]);
 
   const endGame = () => {
     clearInterval(timerId);
-    setIsGameOver(true);
+    setMoleActive(false);
+    //props.setIsGameOver(true);
+    //setIsGameOver(true);
   };
 
   const onPressMole = () => {
@@ -32,30 +48,28 @@ const Square = (props) => {
   };
 
   return (
-    <TouchableOpacity onPress={onPressMole}>
+    <TouchableOpacity
+      onPress={onPressMole}
+      disabled={!moleActive}
+    >
       <Image
         source={
-          moleActive
+          moleActive && !props.isGameOver
             ? require('../assets/mole.png')
             : require('../assets/hole.png')
         }
-        style={moleActive ? styles.mole : styles.square}
+        style={moleActive ? styles.mole : styles.hole}
       ></Image>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  square: {
+  hole: {
     flex: 1,
-    //minWidth: 60,
-    //minHeight: 100,
     width: 80,
     height: 80,
     margin: 10,
-    //borderColor: 'black',
-    //borderStyle: 'solid',
-    //backgroundColor: 'red',
     alignItems: 'center',
     alignContent: 'center',
     justifyContent: 'center',
@@ -66,8 +80,6 @@ const styles = StyleSheet.create({
   },
   mole: {
     flex: 1,
-    //minWidth: 60,
-    //minHeight: 100,
     width: 80,
     height: 80,
     margin: 10,
